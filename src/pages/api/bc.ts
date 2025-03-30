8400d239bbf9abfcdefe1f3f29ffa08b4fb606d1
kbyokin <kbyokin@gmail.com>
2025-03-30 14:34:23 +0900

    Add Fabric Client

---
+++ src/pages/api/bc.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { initializeFabric } from "../../../lib/fabricClient";
+import { TextDecoder } from 'util'; 
+
+const utf8Decoder = new TextDecoder();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+    const contract = await initializeFabric();
+    const resultBytes = await contract.evaluateTransaction("GetAllMedicines");
+    const resultJson = utf8Decoder.decode(resultBytes);
+    const result: any = JSON.parse(resultJson);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to initialize Fabric" });
+  }
+}
